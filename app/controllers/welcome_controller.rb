fbc74acf319510ef6d03e3d9215370d9204e6093
James Conroy <jamespc167@gmail.com>
2015-05-20 11:33:21 -0500

    adds search to front page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,21 @@
 class WelcomeController < ApplicationController
   def index
+
   end
+
+  def wish
+    if movie = Movie.find(params[:movie_id])
+      entry = WatchListEntry.create({
+        user: current_user,
+        movie: movie
+      })
+    end
+    redirect_to '/profile'
+  end
+
+  def destroy_wish
+    WatchListEntry.destroy(params[:id])
+    redirect_to '/profile'
+  end
+
 end
