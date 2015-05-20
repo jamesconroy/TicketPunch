100d499ffe76fd1084655803f8ae42ac653a0771
Tyler Geneva <ctg55@yahoo.com>
2015-05-20 11:35:05 -0500

    uniquness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   has_secure_password
 
+   validates :username, uniqueness: true
+   validates :email, uniqueness: true
+   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+
   has_many :watch_list_entries
   has_many :movies, through: :watch_list_entries
 
